b2efe79a42512aabafcf471b8e71daca07918039
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-06-18 12:13:59 +0700

    initial create controllers uidesummit

---
+++ app/Controllers/Auth.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class Auth extends BaseController
+{
+    public function login()
+    {
+        return view('welcome_message');
+    }
+
+
+    public function register()
+    {
+        return view('welcome_message');
+    }
+}
--- app/Controllers/Event.php
+++ app/Controllers/Event.php
@@ -2,7 +2,7 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class Event extends BaseController
 {
     public function index()
     {
