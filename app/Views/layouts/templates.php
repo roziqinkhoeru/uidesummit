8ed11b3a2331344d23540dfb48d4495d4573d3c7
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-06-24 11:03:22 +0700

    Finish logout modal

--- app/Views/layouts/templates.php
+++ app/Views/layouts/templates.php
@@ -160,7 +160,7 @@
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">
                         Cancel
                     </button>
-                    <a class="btn btn-primary" href="/admin/logout">Logout</a>
+                    <a class="btn btn-danger" href="/admin/logout">Logout</a>
                 </div>
             </div>
         </div>
