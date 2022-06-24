84f382fe245d7c69cb877d3b943434ae7965d254
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-06-24 11:24:11 +0700

    Update footer admin

--- app/Views/layouts/templates.php
+++ app/Views/layouts/templates.php
@@ -128,7 +128,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
+                        <span>Copyright &copy; Uidesummit Admin 2022</span>
                     </div>
                 </div>
             </footer>
