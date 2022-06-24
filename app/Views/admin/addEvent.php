9cf4c54e10231e48821308648cb4886ac09d4512
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-06-24 13:51:12 +0700

    Update style add event

--- app/Views/admin/addEvent.php
+++ app/Views/admin/addEvent.php
@@ -6,10 +6,10 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <div class="d-flex justify-content-between align-items-center">
+    <div class="d-flex justify-content-between align-items-center mb-4">
         <div>
             <h1 class="h3 mb-2 text-gray-800">Add events</h1>
-            <p class="mb-4">A form to add new event data to be held.</p>
+            <p>A form to add new event data to be held.</p>
         </div>
         <div>
             <a href="/event" class="btn d-flex align-items-center">
