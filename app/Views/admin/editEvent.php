b78c7179934a9bec0579a291de70876911db2ba0
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-06-24 13:51:41 +0700

    Update style edit event

--- app/Views/admin/editEvent.php
+++ app/Views/admin/editEvent.php
@@ -6,10 +6,10 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <div class="d-flex justify-content-between align-items-center">
+    <div class="d-flex justify-content-between align-items-center mb-4">
         <div>
             <h1 class="h3 mb-2 text-gray-800">Edit events</h1>
-            <p class="mb-4">Form to change the event data to be held.</p>
+            <p>Form to change the event data to be held.</p>
         </div>
         <div>
             <a href="/event" class="btn d-flex align-items-center">
