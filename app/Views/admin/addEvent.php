7f281b599d1e4dd86d29bc848589154f79053add
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-06-24 13:14:38 +0700

    Finish validate and view add event

--- app/Views/admin/addEvent.php
+++ app/Views/admin/addEvent.php
@@ -16,7 +16,7 @@
         </div>
 
         <div class="card-body">
-            <form action="/event/saveEvent" method="post" enctype="multipart/form-data">
+            <form action="/event/saveEvent" method="post" enctype="multipart/form-data" id="formAddEvent">
                 <!-- name event -->
                 <div class="row mb-3">
                     <label for="inputEmail" class="col-sm-2 col-form-label">Name event</label>
@@ -40,21 +40,71 @@
                 </div>
                 <!-- image event -->
                 <div class="row mb-5">
-                    <label for="inpitImage" class="col-sm-2 col-form-label">Image event</label>
+                    <label for="inputImage" class="col-sm-2 col-form-label">Image event</label>
                     <div class="col-sm-10">
-                        <div class="input-group">
-                            <input type="file" class="form-control" id="imageEvent" name="imageEvent">
-                            <label class="input-group-text" for="imageEvent">Upload</label>
-                        </div>
+                        <input type="file" class="form-control" id="imageEvent" name="imageEvent">
                     </div>
                 </div>
                 <div class="d-flex justify-content-end">
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <a href="/event" class="btn btn-secondary">Cancel</a>
+                    <button type="submit" class="btn btn-primary ml-3" id="buttonAddEvent">Submit</button>
                 </div>
             </form>
         </div>
     </div>
 </div>
 
+<?= $this->endSection(); ?>
+
+<?= $this->section('scriptAdmin'); ?>
+
+<!-- jquery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- jquery validate -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js" integrity="sha512-37T7leoNS06R80c8Ulq7cdCDU5MNQBwlYoy1TX/WUsLFC2eYNqtKlV0QjH7r8JpG/S0GUMZwebnVFLPd6SU5yg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/additional-methods.js"></script>
+
+<script>
+    // validatePassword with JQuery
+    // feedback form
+    const exclamationCircle = "<i class='fa-solid fa-circle-exclamation'></i>";
+
+    $(document).ready(function() {
+        $("#formAddEvent").validate({
+            rules: {
+                nameEvent: {
+                    required: true,
+                },
+                dateEvent: {
+                    required: true,
+                },
+                informationEvent: {
+                    required: true,
+                },
+                imageEvent: {
+                    required: true,
+                },
+            },
+            messages: {
+                nameEvent: {
+                    required: exclamationCircle + " Name event is required",
+                },
+                dateEvent: {
+                    required: exclamationCircle + " Date event is required",
+                },
+                informationEvent: {
+                    required: exclamationCircle + " Information event is required",
+                },
+                imageEvent: {
+                    required: exclamationCircle + " Image event is required",
+                },
+            },
+        });
+
+        $("#buttonAddEvent").on("click", () => {
+            console.log($("#formAddEvent").valid());
+        });
+    });
+</script>
 
 <?= $this->endSection();
--- app/Views/layouts/templates.php
+++ app/Views/layouts/templates.php
@@ -222,6 +222,8 @@
 
     <!-- fontawesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <?= $this->renderSection('scriptAdmin'); ?>
 </body>
 
 </html>
